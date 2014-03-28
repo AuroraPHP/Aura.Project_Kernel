ee4b8adbf5d783e46fa8b087911d581b7cef58bc
David Stanley <davidstanley01@gmail.com>
2014-03-28 00:36:14 -0700

    adding facades

--- src/ProjectContainer.php
+++ src/ProjectContainer.php
@@ -1,40 +1,41 @@
 <?php
 /**
- * 
+ *
  * This file is part of Aura for PHP.
- * 
+ *
  * @package Aura.Project_Kernel
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  */
 namespace Aura\Project_Kernel;
 
 use Aura\Di\Container;
 use Aura\Di\Factory;
 use Aura\Includer\Includer;
+use Auroraphp\Facades\Facade;
 
 /**
- * 
+ *
  * Builder to create a DI container for the project.
- * 
+ *
  * @package Aura.Project_Kernel
- * 
+ *
  */
 class ProjectContainer
 {
     /**
-     * 
+     *
      * Creates and returns a new Container object.
-     * 
+     *
      * There's a lot to hate about this method. It touches the file system
      * directly, it uses the created DI container as a service locator to
      * write to a log, etc.  The only plea I can make is that it is bootstrap
      * code; it's either here, or in a script somewhere, and this is easier
      * to call and test.
-     * 
+     *
      * @return Container
-     * 
+     *
      */
     public static function factory(
         $base,
@@ -48,30 +49,34 @@ class ProjectContainer
 
         // create the project information object
         $project = new Project($base, $env, $installed);
-        
+
         // create the container and set services
         $di = new Container(new Factory);
         $di->set('loader', $loader);
         $di->set('project', $project);
-        
+
         // create a project config object
         $includer = new Includer;
         $project_config = new ProjectConfig($project, $di, $includer);
-        
+
+        // Load facades
+        Facade::registerAliases();
+        Facade::setFacadeApplication($di);
+
         // two-stage config: load definitions, lock, and load modifications
         $project_config->load('define');
-        $di->lock();
+        Facade::getInstance()->lock();
         $project_config->load('modify');
-        
+
         // log debug messages from config?
         if ($log_service) {
-            $logger = $di->get($log_service);
+            $logger = $app->get($log_service);
             foreach ($project_config->getDebug() as $message) {
                 $logger->debug($message);
             }
         }
-        
+
         // done! return the container.
-        return $di;
+        return Facade::getInstance();
     }
 }
